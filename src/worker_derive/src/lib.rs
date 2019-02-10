5c495525c9c1dc405ad6ed2e94f07d1913ff9fae
GHL <ghliku@gmail.com>
2019-02-10 17:08:49 -0500

    sample signature added

--- src/worker_derive/src/lib.rs
+++ src/worker_derive/src/lib.rs
@@ -1,3 +1,17 @@
+extern crate proc_macro;
+
+use crate::proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+mod worker_derive {
+    pub fn worker_derive(input: TokenStream) -> TokenStream {
+        let ast = syn::parse(input).unwrap();
+
+        impl_worker_methods(&ast);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
